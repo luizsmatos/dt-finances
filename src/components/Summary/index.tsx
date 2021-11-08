814c4b568a9b035d686c1dd5853320bc1fde3345
luizsmatos <gusstavopdm@gmail.com>
2021-11-08 14:43:20 -0300

    feat: Soma dos valores de depositos, saidas e total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,6 +7,30 @@ import totalImg from '../../assets/total.svg';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+  
+    if(transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  })
+
+  const formatNumber = (transactionAmount: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(transactionAmount)
+  }
   return (
     <Container>
       <div>
@@ -14,21 +38,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{formatNumber(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>{formatNumber(summary.withdraws)}</strong>
       </div>
       <div>
         <header>
           <p>Entradas</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{formatNumber(summary.total)}</strong>
       </div>
     </Container>
   );
